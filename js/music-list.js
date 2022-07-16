1f9ae0fb5ae41e502dc7d6387390e0496b49e9e9
Jay Vinay <namgirijayvinay@gmail.com>
2022-07-16 11:10:56 +0530

    gggg

---
+++ js/music-list.js
@@ -0,0 +1,35 @@
+let allMusic = [
+
+    {
+        name: "Baby wont you tell me",
+        artist: "Geeta Kiranmai",
+        img: "music-1.jpg",
+        src: "music-1"
+    },
+    {
+        name: "Muvvala navvakala",
+        artist: "Geeta Kiranmai",
+        img: "music-2.jpg",
+        src: "music-2"
+    },
+    {
+        name: "Hey ninnila",
+        artist: "Geeta Kiranmai",
+        img: "music-3.jpg",
+        src: "music-3"
+    },
+    {
+        name: "Em antaro",
+        artist: "Geeta Kiranmai",
+        img: "music-4.jpg",
+        src: "music-4"
+    },
+    {
+        name: "Chali chaliga allindhi",
+        artist: "Geeta Kiranmai",
+        img: "music-5.jpg",
+        src: "music-5"
+    },
+    
+    
+    ];
